55a410fe894d75508e49901962cb8f4b0efe22b3
Soundar Rathinasamy <railslauncher@gmail.com>
2020-04-21 02:11:15 +0530

    Add method to get sleep logs

--- lib/fitgem/sleep.rb
+++ lib/fitgem/sleep.rb
@@ -21,6 +21,20 @@ module Fitgem
       get("/user/#{@user_id}/sleep/date/#{format_date(start_date)}/#{format_date(end_date)}.json")
     end
 
+    # Get sleep data with pagination support
+    def sleep_logs(opts)
+      valid_options = [:before_date, :after_date, :sort, :limit, :offset]
+      opts.slice!(*valid_options)
+
+      before_date = opts[:before_date]
+      after_date = opts[:after_date]
+      opts[:before_date] = format_date(before_date) if before_date
+      opts[:after_date] = format_date(after_date) if after_date
+      query = opts.empty? ? '' : "?#{to_query(opts)}"
+
+      get("/user/#{@user_id}/sleep/list.json#{query}")
+    end
+
     # ==========================================
     #          Sleep Logging Methods
     # ==========================================
@@ -54,6 +68,12 @@ module Fitgem
     # @since v0.4.0
     def delete_sleep_log(sleep_log_id)
       delete("/user/#{@user_id}/sleep/#{sleep_log_id}.json")
+    end
+
+    private
+
+    def to_query(hash)
+      URI.encode(hash.map {|k,v| "#{k}=#{v}" }.join('&'))
     end
   end
 end
